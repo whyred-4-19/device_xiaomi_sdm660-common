881ed05f574513ecaaaa94163c9cc2d05199bc69
jefinhodatnt <jefiinhorelikia@gmail.com>
2020-12-12 02:21:13 -0300

    sdm660-common: Build OP Screen Recorder
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -435,3 +435,7 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
+
+# Recorder
+PRODUCT_PACKAGES += \
+    OPScreenRecorder
