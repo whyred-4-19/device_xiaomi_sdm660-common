e75e75b1c852425cde1725ff2e96758e61430a37
predator112 <65210380+predator112@users.noreply.github.com>
2021-06-18 18:10:45 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
 
 # rm -rf
-rm -rf hardware/qcom/wlan
 rm -rf hardware/qcom-caf/msm8998/display
 rm -rf hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
@@ -8,9 +7,9 @@ rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
-git clone https://github.com/SDM660-Project/android_hardware_qcom_wlan hardware/qcom/wlan
+#git clone https://github.com/SDM660-Project/android_hardware_qcom_wlan hardware/qcom/wlan
 git clone https://github.com/ChrisW444/vendor_XiaomiParts -b 4.19-kernel vendor/XiaomiParts
-git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
+git clone https://github.com/whyred-4-19/vendor_power_4.19 vendor/qcom/opensource/power
 git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_data-ipa-cfg-mgr vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/whyred-4-19/hal_display_4.19 hardware/qcom-caf/msm8998/display
 git clone https://github.com/whyred-4-19/hal_audio_4.19 hardware/qcom-caf/msm8998/audio
